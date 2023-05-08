f1a35ef88521bb05c965c2687caf54e97d881ada
Nouster <djebali.mohamed88@gmail.com>
2023-05-08 15:30:00 +0200

    new package to retrieve data from Sony API

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,29 +2,25 @@
 
 namespace App\Controller;
 
-use App\Entity\Game;
-use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Tustin\PlayStation\Client;
 
 class HomePageController extends AbstractController
 {
+
     #[Route('/', name: 'app_home_page')]
-    public function index(GameRepository $gameRepository): Response
+    public function index(Client $client): Response
     {
-        $games = $gameRepository->findAll();
+        $client->loginWithNpsso("X1k55IvThFTlXSDoRgWtmvEB1ff8DzQ2OhLpflQUXWRgB3YGoQbHefqu4U11r0hw");
+        $me = $client->users()->me();
 
-        return $this->render('home_page/index.html.twig', [
-            'games' => $games,
+        $store = $me->gameList();
+
+        return $this->render('home_page/gamelist.html.twig', [
+            'store' => $store,
         ]);
     }
 
-    #[Route('/game/{id}', name: 'game_item')]
-    public function item(Game $game): Response
-    {
-        return $this->render('home_page/game.html.twig', [
-            'game' => $game
-        ]);
-    }
 }
